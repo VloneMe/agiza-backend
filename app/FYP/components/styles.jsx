2eb955d294e602fbf72d4c8de49fdbeda016ae21
rasuliomari <rasuli707046@gmail.com>
2024-05-24 04:16:47 +0300

    style updates

--- app/FYP/components/styles.jsx
+++ app/FYP/components/styles.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { View, Text, Image, TextInput } from "react-native";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import Constants from "expo-constants";
 
 const StatusBarHeight = Constants.statusBarHeight;
@@ -65,4 +65,34 @@ export const StyledTextInput = styled.TextInput`
     margin-vertical: 3px;
     margin-bottom: 10px;
     color: ${tertiary};
+`;
+
+export const StyledInputLabel = styled.Text`
+    color: ${tertiary};
+    font-size: 13px;
+    text-align: left;
+`;
+
+export const LeftIcon = styled.View`
+    left: 15px;
+    top: 38px;
+    position: absolute;
+    z-index: 1;
+`;
+
+export const RightIcon = styled.TouchableOpacity`
+    right: 15px;
+    top: 38px;
+    position: absolute;
+    z-index: 1;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+    padding: 15px;
+    background-color: ${brand};
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    margin-vertical: 5px;
+    height: 60px;
 `;
