4a9cacdabb5af5b302dc9024eb95e0b39b684bd6
rasuliomari <rasuli707046@gmail.com>
2024-05-25 13:30:24 +0300

    now signup page looking very good

--- app/FYP/app/Signup.jsx
+++ app/FYP/app/Signup.jsx
@@ -40,99 +40,102 @@ import {
 const { brand, darkLight, primary } = Colors;
 
 
-
+// keyboard avoiding view
+import KeyboardAvoidingWrapper from './../components/KeyboardAvoidingWrapper';
 
 const Signup = () => {
     const [hidePassword, setHidePassword] = useState(true);
 
   return (
-    <StyledContainer>
-        <StatusBar style="dark" />
-        <InnerContainer>
-            {/* <PageLogo resizeMode="cover" source={require("./../assets/image/img1.png")} /> */}
-            <PageTitle>aGIZA</PageTitle>
-            <SubTitle>Account Signup</SubTitle>
-            <Formik 
-                initialValues={{ fullName: '', email: '', PhoneNumber: '', password: '', confirmPassword: ''}}
-                onSubmit={(values) => {
-                    console.log(values);
-                }}
-            >
-                {({handleChange, handleBlur, handleSubmit, values}) => (<StyledFormArea>
-                    <MyTextInput 
-                        label="Full Name"
-                        icon="person"
-                        placeholder="Omari Rasuli"
-                        placeholderTextColor={darkLight}
-                        onChangeText={handleChange('fullName')}
-                        onBlur={handleBlur('fullName')}
-                        value={values.fullName}
-                    />
-                    <MyTextInput 
-                        label="Email Address"
-                        icon="mail"
-                        placeholder="rasuliomari4@gmail.com"
-                        placeholderTextColor={darkLight}
-                        onChangeText={handleChange('email')}
-                        onBlur={handleBlur('email')}
-                        value={values.email}
-                        keyboardType="email-address"
-                    />
-                    <MyTextInput 
-                        label="Phone Number"
-                        icon="plus"
-                        placeholder="+255 657707046"
-                        placeholderTextColor={darkLight}
-                        onChangeText={handleChange('PhoneNumber')}
-                        onBlur={handleBlur('PhoneNumber')}
-                        value={values.PhoneNumber}
-                    />
-                    <MyTextInput 
-                        label="Password"
-                        icon="lock"
-                        placeholder="* * * * * * * *"
-                        placeholderTextColor={darkLight}
-                        onChangeText={handleChange('password')}
-                        onBlur={handleBlur('password')}
-                        value={values.password}
-                        secureTextEntry={hidePassword}
-                        isPassword={true}
-                        hidePassword={hidePassword}
-                        setHidePassword={setHidePassword}
-                    />
-                    <MyTextInput 
-                        label="Confirm Password"
-                        icon="lock"
-                        placeholder="* * * * * * * *"
-                        placeholderTextColor={darkLight}
-                        onChangeText={handleChange('confirmPassword')}
-                        onBlur={handleBlur('confirmPassword')}
-                        value={values.confirmPassword}
-                        secureTextEntry={hidePassword}
-                        isPassword={true}
-                        hidePassword={hidePassword}
-                        setHidePassword={setHidePassword}
-                    />
-                    <MsgBox>...</MsgBox>
-                    <StyledButton onPress={handleSubmit}>
-                        <ButtonText>Login</ButtonText>
-                    </StyledButton>
-                    <Line />
-                    {/* <StyledButton onPress={handleSubmit}>
-                        <Fontisto name='google' color={primary} size={25} />
-                        <ButtonText google={true} >Sign in with Google</ButtonText>
-                    </StyledButton> */}
-                    <ExtraView>
-                        <ExtraText>Already have an account? </ExtraText>
-                        <TextLink>
-                            <TextLinkContent>Login</TextLinkContent>
-                        </TextLink>
-                    </ExtraView>
-                </StyledFormArea>)}
+    <KeyboardAvoidingWrapper>
+        <StyledContainer>
+            <StatusBar style="dark" />
+            <InnerContainer>
+                {/* <PageLogo resizeMode="cover" source={require("./../assets/image/img1.png")} /> */}
+                <PageTitle>aGIZA</PageTitle>
+                <SubTitle>Account Signup</SubTitle>
+                <Formik 
+                    initialValues={{ fullName: '', email: '', PhoneNumber: '', password: '', confirmPassword: ''}}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                >
+                    {({handleChange, handleBlur, handleSubmit, values}) => (<StyledFormArea>
+                        <MyTextInput 
+                            label="Full Name"
+                            icon="person"
+                            placeholder="Omari Rasuli"
+                            placeholderTextColor={darkLight}
+                            onChangeText={handleChange('fullName')}
+                            onBlur={handleBlur('fullName')}
+                            value={values.fullName}
+                        />
+                        <MyTextInput 
+                            label="Email Address"
+                            icon="mail"
+                            placeholder="rasuliomari4@gmail.com"
+                            placeholderTextColor={darkLight}
+                            onChangeText={handleChange('email')}
+                            onBlur={handleBlur('email')}
+                            value={values.email}
+                            keyboardType="email-address"
+                        />
+                        <MyTextInput 
+                            label="Phone Number"
+                            icon="plus"
+                            placeholder="+255 657707046"
+                            placeholderTextColor={darkLight}
+                            onChangeText={handleChange('PhoneNumber')}
+                            onBlur={handleBlur('PhoneNumber')}
+                            value={values.PhoneNumber}
+                        />
+                        <MyTextInput 
+                            label="Password"
+                            icon="lock"
+                            placeholder="* * * * * * * *"
+                            placeholderTextColor={darkLight}
+                            onChangeText={handleChange('password')}
+                            onBlur={handleBlur('password')}
+                            value={values.password}
+                            secureTextEntry={hidePassword}
+                            isPassword={true}
+                            hidePassword={hidePassword}
+                            setHidePassword={setHidePassword}
+                        />
+                        <MyTextInput 
+                            label="Confirm Password"
+                            icon="lock"
+                            placeholder="* * * * * * * *"
+                            placeholderTextColor={darkLight}
+                            onChangeText={handleChange('confirmPassword')}
+                            onBlur={handleBlur('confirmPassword')}
+                            value={values.confirmPassword}
+                            secureTextEntry={hidePassword}
+                            isPassword={true}
+                            hidePassword={hidePassword}
+                            setHidePassword={setHidePassword}
+                        />
+                        <MsgBox>...</MsgBox>
+                        <StyledButton onPress={handleSubmit}>
+                            <ButtonText>Login</ButtonText>
+                        </StyledButton>
+                        <Line />
+                        {/* <StyledButton onPress={handleSubmit}>
+                            <Fontisto name='google' color={primary} size={25} />
+                            <ButtonText google={true} >Sign in with Google</ButtonText>
+                        </StyledButton> */}
+                        <ExtraView>
+                            <ExtraText>Already have an account? </ExtraText>
+                            <TextLink>
+                                <TextLinkContent>Login</TextLinkContent>
+                            </TextLink>
+                        </ExtraView>
+                    </StyledFormArea>)}
 
-            </Formik>
-        </InnerContainer>
-    </StyledContainer>
+                </Formik>
+            </InnerContainer>
+        </StyledContainer>
+    </KeyboardAvoidingWrapper>
   )
 }
 
