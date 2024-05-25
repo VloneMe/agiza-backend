45df09d2f55bee7e38088a3b0c15072b890caf4c
rasuliomari <rasuli707046@gmail.com>
2024-05-25 13:28:59 +0300

    the updates of index file

--- app/FYP/app/index.jsx
+++ app/FYP/app/index.jsx
@@ -1,15 +1,18 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-import login from './Login';
-import Signup from './Signup';
-import Welcome from './Welcome';
-import Login from './Login';
+// import login from './Login';
+// import Signup from './Signup';
+// import Welcome from './Welcome';
+
+// react navigation stack
+import RootStack from './../navigators/RootStack';
+
 
 
 
 export default function App() {
   return (
-    <Welcome />
+    <RootStack />
   );
 }
