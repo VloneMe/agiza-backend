45e42813c51e4c9cd1707dec6148ab589ba9babe
rasuliomari <rasuli707046@gmail.com>
2024-05-26 02:11:53 +0300

    new updates in layout page

---
+++ app/FYP/app/Layout.jsx
@@ -0,0 +1,49 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+
+import { Formik } from 'formik';
+import { Link } from 'expo-router';
+
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    ButtonText,
+    ExtraView,
+    ExtraText,
+    StyledButton,
+    Colors,
+    } from '../components/styles';
+
+//import icons
+import { Octicons, Fontisto, Ionicons } from '@expo/vector-icons';
+
+//import colors
+const { brand, darkLight, primary } = Colors;
+
+// keyboard avoiding view
+import KeyboardAvoidingWrapper from '../components/KeyboardAvoidingWrapper';
+
+const Layout = () => {
+
+
+  return (
+    <KeyboardAvoidingWrapper>
+        <StyledContainer>
+            <StatusBar style="black" />
+            <InnerContainer>
+                <PageLogo resizeMode="cover" source={require("./../assets/image/img1.png")} />
+                <PageLogo resizeMode="cover" source={require("../assets/image/image2.png")} />
+                <ExtraView>
+                    <ExtraText>GET STARTED SEND AND RECEIVE YOUR PARCEL SAFETLY WITH US</ExtraText>
+                </ExtraView>
+                <StyledButton>
+                    <ButtonText><Link href="/Login">GET STARTED HERE</Link></ButtonText>
+                 </StyledButton>
+            </InnerContainer>
+        </StyledContainer>  
+    </KeyboardAvoidingWrapper>
+    );
+}
+export default Layout
