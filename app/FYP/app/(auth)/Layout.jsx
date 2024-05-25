d39ffd89cd9e803c0d02d760329d9a483d5bb305
rasuliomari <rasuli707046@gmail.com>
2024-05-26 02:18:41 +0300

    layout page of auth setup

---
+++ app/FYP/app/(auth)/Layout.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AythLayout = () => {
+  return (
+    <View>
+      <Text>AythLayout</Text>
+    </View>
+  )
+}
+
+export default AythLayout
