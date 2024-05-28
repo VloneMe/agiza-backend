b2c5096926b4495574d9ec46f830ef6168be5abe
rasuliomari <rasuli707046@gmail.com>
2024-05-29 02:52:22 +0300

    new screen of request initialize

---
+++ app/FYP/app/Request.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Request() {
+  return (
+    <View>
+      <Text>Request Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
