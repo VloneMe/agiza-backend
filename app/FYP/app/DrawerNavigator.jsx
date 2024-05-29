d61d947fdca677f547aab973d7bc52c9050258bc
rasuliomari <rasuli707046@gmail.com>
2024-05-29 03:50:10 +0300

    new page added of drawer navigation

--- app/FYP/app/DrawerNavigator.jsx
+++ app/FYP/app/DrawerNavigator.jsx
@@ -1,18 +1,29 @@
 import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Icon } from 'react-native-elements';
-import { colors } from '../components/global/styles';
+import { Colorsolors, title } from '../components/global/styles';
 import Welcome from './Welcome';
 
-const Drawer = createDrawerNavigator()
+const Drawer = createDrawerNavigator();
 
 
-expport default function DrawerNavigator() {
+export default function DrawerNavigator() {
     return (
         <Drawer.Navigator>
             <Drawer.Screen 
                 name= "Welcome"
                 component={Welcome}
+                options={{
+                    title: 'Welcome',
+                    drawerIcon: ({focused, size}) => 
+                        <Icon
+                            name='home'
+                            type='font-awesome'
+                            color={ focused ? '#7cc' :Colors.grey2 }
+                            size={size}
+                        />,
+                        headerShown: false
+                }}
             />
         </Drawer.Navigator>
     )
