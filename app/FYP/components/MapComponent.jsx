242fc220b9eb453b88398b802da93089c0cd3f11
rasuliomari <rasuli707046@gmail.com>
2024-05-29 23:57:05 +0300

    new commponents of map added

---
+++ app/FYP/components/MapComponent.jsx
@@ -0,0 +1,35 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class MapComponent extends Component {
+  render() {
+    return (
+      <View>
+         <MapView 
+              ref={_map}
+              provider={PROVIDER_GOOGLE}
+              style={styles.map}
+              customMapStyle={mapStyle}
+              showsUserLocation={true}
+              followsUserLocation={true}
+              initialRegion={{...carsAround[0], latitudeDelta: 0.0922, longitudeDelta: 0.0421}}
+              // rotateEnabled={true}
+              // zoomEnabled={true}
+              // toolbarEnabled={true}
+            >
+              {carsAround.map((item, index) => 
+              <MapView.Marker coordinate = {item} key = {index.toString()}>
+                <Image 
+                  source={require('./../assets/kigaridereva.png')}
+                  style = {styles.carsAround}
+                  resizeMode='cover'
+                />
+              </MapView.Marker>
+            )}
+            </MapView>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
