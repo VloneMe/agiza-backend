77f4507d9fea6922485458b96011e16a67be7093
rasuliomari <rasuli707046@gmail.com>
2024-06-09 19:48:39 +0300

    new context added

--- brand/context/context.jsx
+++ brand/context/context.jsx
@@ -1,8 +1,41 @@
-import React, { createContext, useReducer} from "react";
-import { OriginReducer, DestinationReducer } from "../reducers/reducers";
+// import React, { createContext, useReducer} from "react";
+// import { OriginReducer, DestinationReducer } from "../reducers/reducers";
 
-export const Origincontext = createContext()
-export const DestinationContext = createContext()
+
+// // Initial state for origin and destination
+// const initialOriginState = {
+//     latitude: 0,
+//     longitude: 0,
+//   };
+  
+//   const initialDestinationState = {
+//     latitude: 0,
+//     longitude: 0,
+//   };
+
+// export const Origincontext = createContext()
+// export const DestinationContext = createContext()
+
+// // Context Providers
+// export const OrigincontextProvider = ({ children }) => {
+//     const [origin, dispatchOrigin] = useReducer(OriginReducer, initialOriginState);
+//     return (
+//       <Origincontext.Provider value={{ origin, dispatchOrigin }}>
+//         {children}
+//       </Origincontext.Provider>
+//     );
+//   };
+  
+//   export const DestinationProvider = ({ children }) => {
+//     const [destination, dispatchDestination] = useReducer(DestinationReducer, initialDestinationState);
+//     return (
+//       <DestinationContext.Provider value={{ destination, dispatchDestination }}>
+//         {children}
+//       </DestinationContext.Provider>
+//     );
+//   };
+
+
 
 export const OrigincontextProvider = (props) => {
 const [origin, dispatchOrigin] = useReducer(OriginReducer, {
