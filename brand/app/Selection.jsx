6d517ae94268b934d877c4b8a5fe62b7d47cc4ea
rasuliomari <rasuli707046@gmail.com>
2024-06-10 19:48:07 +0300

    new commit added to this page

---
+++ brand/app/Selection.jsx
@@ -0,0 +1,238 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, Text, StyleSheet, Dimensions, ScrollView, Image, FlatList, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react';
+import { Link } from 'expo-router';
+import { Formik } from 'formik';
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle,
+    SubTitle,
+    StyledFormArea,
+    StyledTextInput,
+    StyledInputLabel,
+    LeftIcon,
+    RightIcon,
+    ButtonText,
+    MsgBox,
+    Line,
+    ExtraView,
+    ExtraText,
+    TextLink,
+    TextLinkContent,
+    StyledButton,
+    } from '../components/styles';
+
+import { Octicons, Fontisto, Ionicons } from '@expo/vector-icons';
+// keyboard avoiding view
+import KeyboardAvoidingWrapper from '../components/KeyboardAvoidingWrapper';
+import { Colors, parameters } from '../components/global/styles';
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const Selection = () => {
+  return (
+    <View style={styles.view5}>
+            <View style={styles.view6}>
+              <View style={styles.view7}>
+                  <Icon 
+                      type = "material-community"
+                      name = "map-marker"
+                      color = {Colors.black}
+                      size = {22}
+                  />
+              </View>
+              <View>
+                  <Text style={{fontSize:18, color:Colors.black}}>Mliman City Mall</Text>
+                  <Text style={{color:Colors.grey3}}>Dar es Salaam</Text>
+              </View>
+            </View>
+            <View>
+                    <Icon 
+                      type = "material-community"
+                      name = "chevron-right"
+                      color = {Colors.grey}
+                      size = {26}
+                    />
+            </View>
+          </View>
+  )
+}
+
+export default Selection
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:Colors.white,
+        paddingBottom:30,
+        paddingTop:parameters.statusBarHeight
+    },
+    header:{
+      backgroundColor:Colors.blue,
+      height:parameters.headerHeight,
+      alignItems:"flex-start"
+    },
+    
+    image1:{
+     
+      height:100,
+      width:100,
+    
+    },
+    
+    image2:{height:60,width:60,
+            borderRadius:30,
+          },
+    
+    home:{
+     backgroundColor:Colors.blue,
+     paddingLeft:20,
+     
+    },
+    
+    text1:{
+     color:Colors.white,
+     fontSize:21,
+     paddingBottom:20,
+     paddingTop:20
+    },
+    
+    text2:{
+     color:Colors.white,
+     fontSize:16
+    },
+    
+    view1:{
+     flexDirection:"row",
+     flex:1,
+     paddingTop:30
+    },
+    
+    button1:{
+      height:40,
+      width:150,
+      backgroundColor:Colors.black,
+      borderRadius:20,
+      alignItems:"center",
+      justifyContent:"center",
+      marginTop:20
+    },
+    
+    button1Text:{
+     color:Colors.white,
+     fontSize:17,
+     marginTop:-2
+    
+    },
+    card:{
+     alignItems:"center",
+     margin:SCREEN_WIDTH/22
+    
+    },
+    
+    view2:{marginBottom:5,
+          borderRadius:15,
+          backgroundColor:Colors.grey6
+        },
+    
+        title:{
+          color:Colors.black,
+          fontSize:16
+        },
+    view3:{flexDirection:"row",
+             marginTop :5,
+             height:50,
+             backgroundColor:Colors.grey6,
+             alignItems:"center",
+             justifyContent:"space-between",
+            marginHorizontal:15
+            
+             },
+    text3:{marginLeft:15,
+            fontSize:20,
+            color:Colors.black
+      },
+    
+    view4:{ flexDirection:"row",
+            alignItems:"center",
+            marginRight:15,
+            backgroundColor:"white",
+            paddingHorizontal:10,
+            paddingVertical:2,
+            borderRadius:20
+            },
+    
+    view5:{ flexDirection:"row",
+    alignItems:"center",
+    backgroundColor:"white",
+    paddingVertical:25,
+    justifyContent:"space-between",
+    marginHorizontal:15,
+    borderBottomColor:Colors.grey4,
+    borderBottomWidth:1,
+    flex:1
+    },
+    
+    view6:{
+    
+    
+    alignItems:"center",
+    flex:5,
+    flexDirection:"row"
+    },
+    view7:{
+    backgroundColor:Colors.grey6,
+    height:40,
+    width:40,
+    borderRadius:20,
+    alignItems:"center",
+    justifyContent:"center",
+    marginRight:20
+    
+    },
+    
+    map:{
+       
+    height: 150,
+     marginVertical: 0,
+     width:SCREEN_WIDTH*0.92
+    },
+    
+    text4:{ fontSize:20,
+          color:Colors.black,
+          marginLeft:20,
+          marginBottom:20
+        },
+    
+    icon1:  {marginLeft:10,
+           marginTop:5
+          },
+
+    view8: {flex:4,
+          marginTop:-25
+        } ,
+    carsAround: {
+    width: 28,
+    height: 14,
+    
+    }, 
+    
+    location: {
+      width: 16,
+      height: 16,
+      borderRadius:8,
+      backgroundColor:Colors.blue,
+      alignItems:"center",
+      justifyContent:"center"
+      
+      }, 
+      
+    view9:{width:4,
+    height:4,
+    borderRadius:2,
+    backgroundColor:"white"
+    }
+
+
+})
