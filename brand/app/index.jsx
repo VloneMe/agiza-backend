28b4144ac2b325bb15b284bb7b06041dfddcf999
rasuliomari <rasuli707046@gmail.com>
2024-06-11 18:00:07 +0300

    new words added in index page

--- brand/app/index.jsx
+++ brand/app/index.jsx
@@ -35,17 +35,12 @@ const Layout = () => {
             <InnerContainer>
                 <PageLogo resizeMode="cover" source={require("./../assets/img1.png")} />
                 <ExtraView>
-                    <ExtraText>GET STARTED SEND AND RECEIVE YOUR PARCEL SAFETLY WITH US</ExtraText>
-                </ExtraView>
-                <ExtraView>
-                    <ExtraText>ONGEZENI MANENO YAKUVUTIA ILI MTUMIAJI AVUTIWE KUITUMIA MANENO SINA YAKUVUTIA MPAKA SASAA </ExtraText>
+                    <ExtraText>Ditch the stress of deliveries!
+                    aGIZA puts the power in your hands.  Order pickups and deliveries with a few taps, track your parcels in real-time, and enjoy the peace of mind that comes with knowing your package is in good hands.  Stop wasting time waiting in lines or dealing with unreliable couriers. Download aGIZA today and experience a delivery revolution!</ExtraText>
                 </ExtraView>
                 <PageLogo resizeMode="cover" source={require("./../assets/image2.png")} />
                 <ExtraView>
-                    <ExtraText>GET STARTED SEND AND RECEIVE YOUR PARCEL SAFETLY WITH US</ExtraText>
-                </ExtraView>
-                <ExtraView>
-                    <ExtraText>ONGEZENI MANENO YAKUVUTIA ILI MTUMIAJI AVUTIWE KUITUMIA MANENO SINA YAKUVUTIA MPAKA SASAA </ExtraText>
+                    <ExtraText>Download aGIZA, your one-stop app for effortless parcel delivery! Get your packages picked up and delivered on your schedule, with real-time tracking and exclusive discounts. Whether you're a busy professional, a frequent online shopper, or just someone who values convenience, aGIZA makes your life easier. Sign up today and experience the future of delivery!</ExtraText>
                 </ExtraView>
                 <StyledButton>
                     <Link href="/Login">
@@ -58,3 +53,4 @@ const Layout = () => {
     );
 }
 export default Layout
+{/* <ExtraText>GET STARTED SEND AND RECEIVE YOUR PARCEL SAFETLY WITH US</ExtraText> */}
