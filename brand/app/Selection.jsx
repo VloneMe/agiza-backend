66924d3133285f71b4362ef325d4ccd559a9dcdc
rasuliomari <rasuli707046@gmail.com>
2024-06-11 18:13:48 +0300

    new page of selection modified to look good

--- brand/app/Selection.jsx
+++ brand/app/Selection.jsx
@@ -33,224 +33,246 @@ const SCREEN_WIDTH = Dimensions.get('window').width;
 
 const Selection = () => {
   return (
-       <ScrollView bounces={false}>
-        <View style={styles.home}>
-          <Text style={styles.text1}>Welcome to aGIZA</Text>
-          <View style={styles.view1}>
-            <View style={styles.view8}>
-              <Text style={styles.text2}>Please choose the ride you want</Text>
-            </View>
-          </View>
-        </View>
 
-        <TouchableOpacity style={{}}>
-        <View style={styles.view5}>
-            <View style={styles.view6}>
-              <View style={styles.view7}>
-                  <Icon 
-                      type = "material-community"
-                      name = "bed-single"
-                      color = {Colors.black}
-                      size = {22}
-                  />
-              </View>
-              <View>
-                  <Text style={{fontSize:18, color:Colors.black}}>Mliman City Mall</Text>
-                  <Text style={{color:Colors.grey3}}>Dar es Salaam</Text>
-              </View>
-            </View>
-            <View>
-                    <Icon 
-                      type = "material-community"
-                      name = "chevron-right"
-                      color = {Colors.grey}
-                      size = {26}
-                    />
-            </View>
-          </View>
-          </TouchableOpacity>
-          </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.header}>Select Your Ride</Text>
+
+      <TouchableOpacity style={styles.option}>
+        <Image source={require('./../assets/pikipiki.jpeg')} style={styles.image} />
+        <Text style={styles.optionText}>Motorcycle</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.option}>
+        <Image source={require('./../assets/kirikuu.jpeg')} style={styles.image} />
+        <Text style={styles.optionText}>Small Van</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.option}>
+        <Image source={require('./../assets/fuso.jpeg')} style={styles.image} />
+        <Text style={styles.optionText}>Big Van</Text>
+      </TouchableOpacity>
+    </View>
+
+
+      //  <ScrollView bounces={false}>
+      //   <View style={styles.home}>
+      //     <Text style={styles.text1}>Welcome to aGIZA</Text>
+      //     <View style={styles.view1}>
+      //       <View style={styles.view8}>
+      //         <Text style={styles.text2}>Please choose the ride you want</Text>
+      //       </View>
+      //     </View>
+      //   </View>
+      //     </ScrollView>
   )
 }
 
 export default Selection
 
+
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:Colors.white,
-        paddingBottom:30,
-        paddingTop:parameters.statusBarHeight
-    },
-    header:{
-      backgroundColor:Colors.blue,
-      height:parameters.headerHeight,
-      alignItems:"flex-start"
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  option: {
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 10,
+  },
+  optionText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+// const styles = StyleSheet.create({
+//     container:{
+//         flex:1,
+//         backgroundColor:Colors.white,
+//         paddingBottom:30,
+//         paddingTop:parameters.statusBarHeight
+//     },
+//     header:{
+//       backgroundColor:Colors.blue,
+//       height:parameters.headerHeight,
+//       alignItems:"flex-start"
+//     },
     
-    image1:{
+//     image1:{
      
-      height:100,
-      width:100,
+//       height:100,
+//       width:100,
     
-    },
+//     },
     
-    image2:{height:60,width:60,
-            borderRadius:30,
-          },
+//     image2:{height:60,width:60,
+//             borderRadius:30,
+//           },
     
-    home:{
-     backgroundColor:Colors.blue,
-     paddingLeft:20,
+//     home:{
+//      backgroundColor:Colors.blue,
+//      paddingLeft:20,
      
-    },
+//     },
     
-    text1:{
-     color:Colors.white,
-     fontSize:21,
-     paddingBottom:20,
-     alignContent:"center",
-     textAlign:"center",
-     paddingTop:20
-    },
+//     text1:{
+//      color:Colors.white,
+//      fontSize:21,
+//      paddingBottom:20,
+//      alignContent:"center",
+//      textAlign:"center",
+//      paddingTop:20
+//     },
     
-    text2:{
-     color:Colors.white,
-     fontSize:16,
-      paddingBottom:10,
-      textAlign:"center"
-    },
+//     text2:{
+//      color:Colors.white,
+//      fontSize:16,
+//       paddingBottom:10,
+//       textAlign:"center"
+//     },
     
-    view1:{
-     flexDirection:"row",
-     flex:1,
-     paddingTop:30
-    },
+//     view1:{
+//      flexDirection:"row",
+//      flex:1,
+//      paddingTop:30
+//     },
     
-    button1:{
-      height:40,
-      width:150,
-      backgroundColor:Colors.black,
-      borderRadius:20,
-      alignItems:"center",
-      justifyContent:"center",
-      marginTop:20
-    },
+//     button1:{
+//       height:40,
+//       width:150,
+//       backgroundColor:Colors.black,
+//       borderRadius:20,
+//       alignItems:"center",
+//       justifyContent:"center",
+//       marginTop:20
+//     },
     
-    button1Text:{
-     color:Colors.white,
-     fontSize:17,
-     marginTop:-2
+//     button1Text:{
+//      color:Colors.white,
+//      fontSize:17,
+//      marginTop:-2
     
-    },
-    card:{
-     alignItems:"center",
-     margin:SCREEN_WIDTH/22
+//     },
+//     card:{
+//      alignItems:"center",
+//      margin:SCREEN_WIDTH/22
     
-    },
+//     },
     
-    view2:{marginBottom:5,
-          borderRadius:15,
-          backgroundColor:Colors.grey6
-        },
+//     view2:{marginBottom:5,
+//           borderRadius:15,
+//           backgroundColor:Colors.grey6
+//         },
     
-        title:{
-          color:Colors.black,
-          fontSize:16
-        },
-    view3:{flexDirection:"row",
-             marginTop :5,
-             height:50,
-             backgroundColor:Colors.grey6,
-             alignItems:"center",
-             justifyContent:"space-between",
-            marginHorizontal:15
+//         title:{
+//           color:Colors.black,
+//           fontSize:16
+//         },
+//     view3:{flexDirection:"row",
+//              marginTop :5,
+//              height:50,
+//              backgroundColor:Colors.grey6,
+//              alignItems:"center",
+//              justifyContent:"space-between",
+//             marginHorizontal:15
             
-             },
-    text3:{marginLeft:15,
-            fontSize:20,
-            color:Colors.black
-      },
+//              },
+//     text3:{marginLeft:15,
+//             fontSize:20,
+//             color:Colors.black
+//       },
     
-    view4:{ flexDirection:"row",
-            alignItems:"center",
-            marginRight:15,
-            backgroundColor:"white",
-            paddingHorizontal:10,
-            paddingVertical:2,
-            borderRadius:20
-            },
+//     view4:{ flexDirection:"row",
+//             alignItems:"center",
+//             marginRight:15,
+//             backgroundColor:"white",
+//             paddingHorizontal:10,
+//             paddingVertical:2,
+//             borderRadius:20
+//             },
     
-    view5:{ flexDirection:"row",
-    alignItems:"center",
-    backgroundColor:"white",
-    paddingVertical:25,
-    justifyContent:"space-between",
-    marginHorizontal:15,
-    borderBottomColor:Colors.grey4,
-    borderBottomWidth:1,
-    flex:1
-    },
+//     view5:{ flexDirection:"row",
+//     alignItems:"center",
+//     backgroundColor:"white",
+//     paddingVertical:25,
+//     justifyContent:"space-between",
+//     marginHorizontal:15,
+//     borderBottomColor:Colors.grey4,
+//     borderBottomWidth:1,
+//     flex:1
+//     },
     
-    view6:{
+//     view6:{
     
     
-    alignItems:"center",
-    flex:5,
-    flexDirection:"row"
-    },
-    view7:{
-    backgroundColor:Colors.grey6,
-    height:40,
-    width:40,
-    borderRadius:20,
-    alignItems:"center",
-    justifyContent:"center",
-    marginRight:20
+//     alignItems:"center",
+//     flex:5,
+//     flexDirection:"row"
+//     },
+//     view7:{
+//     backgroundColor:Colors.grey6,
+//     height:40,
+//     width:40,
+//     borderRadius:20,
+//     alignItems:"center",
+//     justifyContent:"center",
+//     marginRight:20
     
-    },
+//     },
     
-    map:{
+//     map:{
        
-    height: 150,
-     marginVertical: 0,
-     width:SCREEN_WIDTH*0.92
-    },
+//     height: 150,
+//      marginVertical: 0,
+//      width:SCREEN_WIDTH*0.92
+//     },
     
-    text4:{ fontSize:20,
-          color:Colors.black,
-          marginLeft:20,
-          marginBottom:20
-        },
+//     text4:{ fontSize:20,
+//           color:Colors.black,
+//           marginLeft:20,
+//           marginBottom:20
+//         },
     
-    icon1:  {marginLeft:10,
-           marginTop:5
-          },
+//     icon1:  {marginLeft:10,
+//            marginTop:5
+//           },
 
-    view8: {flex:4,
-          marginTop:-25
-        } ,
-    carsAround: {
-    width: 28,
-    height: 14,
+//     view8: {flex:4,
+//           marginTop:-25
+//         } ,
+//     carsAround: {
+//     width: 28,
+//     height: 14,
     
-    }, 
+//     }, 
     
-    location: {
-      width: 16,
-      height: 16,
-      borderRadius:8,
-      backgroundColor:Colors.blue,
-      alignItems:"center",
-      justifyContent:"center"
+//     location: {
+//       width: 16,
+//       height: 16,
+//       borderRadius:8,
+//       backgroundColor:Colors.blue,
+//       alignItems:"center",
+//       justifyContent:"center"
       
-      }, 
+//       }, 
       
-    view9:{width:4,
-    height:4,
-    borderRadius:2,
-    backgroundColor:"white"
-    }
+//     view9:{width:4,
+//     height:4,
+//     borderRadius:2,
+//     backgroundColor:"white"
+//     }
 
 
-})
+// })
