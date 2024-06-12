fc129d380d34b38f394ff74876c8d55f3e89543d
Vlone Me <francisntima@gmail.com>
2024-06-12 09:23:30 -0400

    Server Changed

--- server/src/models/sync.js
+++
@@ -1,7 +0,0 @@
-const { sequelize } = require('./models');
-
-sequelize.sync({ force: true }).then(() => {
-  console.log('[server]: Database synchronized');
-}).catch(error => {
-  console.error('[server]: Error synchronizing the database:', error);
-});
