8d1453a51a03ea3f16ccc5553e561054a3d62ab7
rasuliomari <rasuli707046@gmail.com>
2024-06-13 02:38:41 +0300

    not working as intended

--- brand/app/Login.jsx
+++ brand/app/Login.jsx
@@ -90,14 +90,10 @@ const Login = () => {
                             <ButtonText><Link href="Welcome">Login</Link></ButtonText>
                         </StyledButton>
                         <Line />
-                        {/* <StyledButton onPress={handleSubmit}>
-                            <Fontisto name='google' color={primary} size={25} />
-                            <ButtonText google={true} >Sign in with Google</ButtonText>
-                        </StyledButton> */}
                         <ExtraView>
                             <ExtraText>Don't have an account already? </ExtraText>
                             <TextLink>
-                                <TextLinkContent>Signup</TextLinkContent>
+                                <TextLinkContent><Text><Link href="/Signup" >Signup</Link></Text></TextLinkContent>
                             </TextLink>
                         </ExtraView>
                     </StyledFormArea>)}
