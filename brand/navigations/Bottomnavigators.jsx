4c3a7d83a948b2e3be509cc60173b09269a47736
rasuliomari <rasuli707046@gmail.com>
2024-06-17 02:07:45 +0300

    new codes of bottom navigatiors

--- brand/navigations/Bottomnavigators.jsx
+++ brand/navigations/Bottomnavigators.jsx
@@ -9,7 +9,8 @@ import Profile from './../app/Profile';
 import Details from './../app/Details';
 
 import { Ionicons } from '@expo/vector-icons';
-import { Stack useRouter} from 'expo-router';
+import { Stack } from 'expo-router';
+import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
 
 
 
@@ -17,20 +18,68 @@ import { Stack useRouter} from 'expo-router';
 const Tab = createBottomTabNavigator();
 const stack = creatNativeStackNavigator();
 
-const router = useRouter(); // Initialize the router
+
 
 const HomeStack = () => {
   return(
     <Stack.Navigator>
       <Stack.Screen name="Welcome" component={Welcome} options={{headerShown: false}} />
-      <Stack.Screen name="Profile" component={Profile} options={router.push('Profile')}/>
+      <Stack.Screen name="Profile" component={Profile} options={({route})=>({title: route.params?.title,})} />
       <Stack.Screen name="Details" component={Details} />
     </Stack.Navigator>
   )
 };
 
 const BottomTabsNavigator = () => {
-  return ()
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {backgroundColor: '#AD40AF'},
+        tabBarInactiveTintColor: '#fff',
+        tabBarActiveTintColor: 'yellow'
+      }}
+    >
+      <Tab.Screen 
+        name="home2"
+        component={HomeStack}
+        options={({route})=>({
+          tabBarStyle:{
+            display: getTabBarVisibility(route),
+            backgroundColor: '#AD40AF',
+          },
+          tabBarIcon: ({color,size})=>(
+            <Ionicons  name="home-outline" color={color} size={size} />
+          ),
+        })}
+      />
+      <Tab.Screen 
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({color,size})=>(
+            <Ionicons  name="person-add-outline" color={color} size={size} />
+          ),
+        }}
+      /><Tab.Screen 
+        name="details"
+        component={Details}
+        options={{
+          tabBarIcon: ({color,size})=>(
+            <Ionicons  name="git-pull-request-outline" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  )
 };
+
+const getTabBarVisibility = route =>{
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Feed';
+  if( routeName == 'Profile'){
+    return 'none';
+  }return 'flex';
+}
 
 export default BottomTabsNavigator;
