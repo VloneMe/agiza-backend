9add3699f02c530b2ca64331e9952b436b254aad
Vlone Me <francisntima@gmail.com>
2024-06-17 06:47:24 -0400

    server update

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -6,7 +6,11 @@ const userSchema = new Schema({
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     phone: { type: String, required: true, unique: true },
-    role: { type: String, enum: ['customer', 'courier', 'admin'], default: 'available', required: true }
+    role: { 
+        type: String, 
+        enum: ['customer', 'courier', 'admin'], 
+        default: 'customer', required: true 
+    }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
