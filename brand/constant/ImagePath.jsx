3ed4ebbefe21645ac78da5d687f0b00705ad3619
Vlone Me <francisntima@gmail.com>
2024-06-23 18:51:36 -0400

    update

--- brand/constant/ImagePath.jsx
+++ brand/constant/ImagePath.jsx
@@ -1,5 +1,5 @@
 export default {
-    icCurLoc: require('./../assets/images/Oval.png'),
+    icCurLoc: require('../assets/images/Oval.png'),
     icGreenMarker: require("./../assets/images/greenMarker.png"),
     greenIndicator: require("./../assets/images/greenIndicator.png"),
     icBike: require("./../assets/images/bike.png"),
