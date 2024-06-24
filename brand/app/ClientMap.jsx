5e526861a10c5b5b13bd0f47952c45d682463a01
rasuliomari <rasuli707046@gmail.com>
2024-06-24 18:51:50 +0300

    taking cordinates

--- brand/app/ClientMap.jsx
+++ brand/app/ClientMap.jsx
@@ -1,49 +1,57 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
 import ImagePath from "./../constant/ImagePath";
+import { getCurrentLocation, locationPermission } from "./../helper/helperFunction";
 import { useRouter } from 'expo-router';
 
 const ClientMap = () => {
-  const mapRef = useRef(null);
-
   const [state, setState] = useState({
     pickupCords: {
       latitude: -6.8226625,
       longitude: 39.30244649999999,
-      latitudeDelta: 0.3924108008642557,
-      longitudeDelta: 0.18297526985406876,
     },
-    dropoffCords: {
-      latitude: -6.6173653,
-      longitude: 39.1235548,
-      latitudeDelta: 0.3924108008642557,
-      longitudeDelta: 0.18297526985406876,
-    },
+    dropoffCords: null,
   });
 
   const { pickupCords, dropoffCords } = state;
 
-  const onPressLocation = () => {
-    {getCordinates: fetchValues}
-  };
+  const mapRef = useRef(null);
+  const router = useRouter(); // Initialize the router
+
+  useEffect(() => {
+    const getLiveLocation = async () => {
+      const locPermissionDenied = await locationPermission();
+      if (!locPermissionDenied) {
+        const { latitude, longitude } = await getCurrentLocation();
+        setState((prevState) => ({
+          ...prevState,
+          pickupCords: {
+            latitude,
+            longitude,
+          },
+        }));
+      }
+    };
+
+    getLiveLocation();
+  }, []);
 
   const fetchValues = (data) => {
-    setState({
-      startingCords: {
-        latitude: data.pickupCords.latitude,
-        longitude: data.pickupCords.longitude,
-      },
-      destinationCords: {
+    setState((prevState) => ({
+      ...prevState,
+      dropoffCords: {
         latitude: data.dropoffCords.latitude,
         longitude: data.dropoffCords.longitude,
-      }
-    });
+      },
+    }));
     console.log("Data===>", data);
-  }
+  };
 
-  const router = useRouter(); // Initialize the router
+  const onPressLocation = () => {
+    router.push('/Destination');
+  };
 
   return (
     <View style={styles.container}>
@@ -51,7 +59,11 @@ const ClientMap = () => {
         <MapView
           provider={PROVIDER_GOOGLE}
           style={StyleSheet.absoluteFill}
-          initialRegion={pickupCords}
+          initialRegion={{
+            ...pickupCords,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
           ref={mapRef}
         >
           <Marker 
@@ -59,43 +71,47 @@ const ClientMap = () => {
             pinColor="green" 
             image={ImagePath.icCurLoc}
           />
-          <Marker 
-            coordinate={dropoffCords} 
-            pinColor="red" 
-            image={ImagePath.icGreenMarker}
-          />
-          <MapViewDirections 
-            origin={pickupCords}
-            destination={dropoffCords}
-            apikey='AIzaSyD6DVLho-QJOqaxGKZ9pDQLYuDkvxlTyuw'
-            strokeWidth={3}
-            strokeColor="hotpink"
-            onReady={result => {
-              if (mapRef.current) {
-                mapRef.current.fitToCoordinates(result.coordinates, {
-                  edgePadding: {
-                    right: 30,
-                    left: 30,
-                    top: 100,
-                    bottom: 300,
-                  },
-                  animated: true,
-                });
-                console.log(`Distance: ${result.distance} km`);
-                console.log(`Duration: ${result.duration} min.`);
-              } else {
-                console.log('MapView reference is not available');
-              }
-            }}
-            onError={(errorMessage) => {
-              console.log('GOT AN ERROR', errorMessage);
-            }}
-          />
+          {dropoffCords && (
+            <Marker 
+              coordinate={dropoffCords} 
+              pinColor="red" 
+              image={ImagePath.icGreenMarker}
+            />
+          )}
+          {dropoffCords && (
+            <MapViewDirections 
+              origin={pickupCords}
+              destination={dropoffCords}
+              apikey='AIzaSyD6DVLho-QJOqaxGKZ9pDQLYuDkvxlTyuw'
+              strokeWidth={3}
+              strokeColor="hotpink"
+              onReady={result => {
+                if (mapRef.current) {
+                  mapRef.current.fitToCoordinates(result.coordinates, {
+                    edgePadding: {
+                      right: 30,
+                      left: 30,
+                      top: 100,
+                      bottom: 300,
+                    },
+                    animated: true,
+                  });
+                  console.log(`Distance: ${result.distance} km`);
+                  console.log(`Duration: ${result.duration} min.`);
+                } else {
+                  console.log('MapView reference is not available');
+                }
+              }}
+              onError={(errorMessage) => {
+                console.log('GOT AN ERROR', errorMessage);
+              }}
+            />
+          )}
         </MapView>
       </View>
       <View style={styles.bottomCard}>
         <Text>Where are you going..?</Text>
-        <TouchableOpacity style={styles.inputStyle} onPress={(onPressLocation) => router.push('/Destination')}>
+        <TouchableOpacity style={styles.inputStyle} onPress={onPressLocation}>
           <Text>Choose your location</Text>
         </TouchableOpacity>
       </View>
@@ -126,146 +142,3 @@ const styles = StyleSheet.create({
 });
 
 export default ClientMap;
-
-
-
-
-
-// screens/ClientMap.js
-// import React, { useRef, useState, useEffect } from "react";
-// import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-// import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
-// import MapViewDirections from "react-native-maps-directions";
-// import ImagePath from "./../constant/ImagePath";
-// import { useNavigation, useRoute } from '@react-navigation/native';
-
-// const ClientMap = () => {
-//   const mapRef = useRef(null);
-//   const navigation = useNavigation();
-//   const route = useRoute();
-
-//   const [state, setState] = useState({
-//     pickupCords: {
-//       latitude: -6.8226625,
-//       longitude: 39.30244649999999,
-//       latitudeDelta: 0.3924108008642557,
-//       longitudeDelta: 0.18297526985406876,
-//     },
-//     dropoffCords: {
-//       latitude: -6.6173653,
-//       longitude: 39.1235548,
-//       latitudeDelta: 0.3924108008642557,
-//       longitudeDelta: 0.18297526985406876,
-//     },
-//   });
-
-//   const { pickupCords, dropoffCords } = state;
-
-//   const setCoordinates = (data) => {
-//     setState((prevState) => ({
-//       ...prevState,
-//       pickupCords: {
-//         latitude: data.pickupCords.latitude,
-//         longitude: data.pickupCords.longitude,
-//         latitudeDelta: 0.3924108008642557,
-//         longitudeDelta: 0.18297526985406876,
-//       },
-//       dropoffCords: {
-//         latitude: data.dropoffCords.latitude,
-//         longitude: data.dropoffCords.longitude,
-//         latitudeDelta: 0.3924108008642557,
-//         longitudeDelta: 0.18297526985406876,
-//       },
-//     }));
-//   };
-
-//   useEffect(() => {
-//     if (route.params?.pickupCords && route.params?.dropoffCords) {
-//       setCoordinates(route.params);
-//     }
-//   }, [route.params]);
-
-//   return (
-//     <View style={styles.container}>
-//       <View style={{ flex: 1 }}>
-//         <MapView
-//           provider={PROVIDER_GOOGLE}
-//           style={StyleSheet.absoluteFill}
-//           initialRegion={pickupCords}
-//           ref={mapRef}
-//         >
-//           <Marker 
-//             coordinate={pickupCords} 
-//             pinColor="green" 
-//             image={ImagePath.icCurLoc}
-//           />
-//           <Marker 
-//             coordinate={dropoffCords} 
-//             pinColor="red" 
-//             image={ImagePath.icGreenMarker}
-//           />
-//           <MapViewDirections 
-//             origin={pickupCords}
-//             destination={dropoffCords}
-//             apikey='AIzaSyD6DVLho-QJOqaxGKZ9pDQLYuDkvxlTyuw'
-//             strokeWidth={3}
-//             strokeColor="hotpink"
-//             onReady={result => {
-//               if (mapRef.current) {
-//                 mapRef.current.fitToCoordinates(result.coordinates, {
-//                   edgePadding: {
-//                     right: 30,
-//                     left: 30,
-//                     top: 100,
-//                     bottom: 300,
-//                   },
-//                   animated: true,
-//                 });
-//                 console.log(`Distance: ${result.distance} km`);
-//                 console.log(`Duration: ${result.duration} min.`);
-//               } else {
-//                 console.log('MapView reference is not available');
-//               }
-//             }}
-//             onError={(errorMessage) => {
-//               console.log('GOT AN ERROR', errorMessage);
-//             }}
-//           />
-//         </MapView>
-//       </View>
-//       <View style={styles.bottomCard}>
-//         <Text>Where are you going..?</Text>
-//         <TouchableOpacity 
-//           style={styles.inputStyle} 
-//           onPress={() => navigation.navigate('Destination', { setCoordinates })}
-//         >
-//           <Text>Choose your location</Text>
-//         </TouchableOpacity>
-//       </View>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   bottomCard: {
-//     backgroundColor: 'white',
-//     width: '100%',
-//     padding: 30,
-//     borderTopEndRadius: 24,
-//     borderTopStartRadius: 24,
-//   },
-//   inputStyle: {
-//     backgroundColor: 'white',
-//     borderRadius: 4,
-//     borderWidth: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     height: 50,
-//     marginTop: 16,
-//   },
-// });
-
-// export default ClientMap;
