b23388c9997c28da2a9a251067d597fb4fa5d6f0
rasuliomari <rasuli707046@gmail.com>
2024-06-24 15:52:04 +0300

    trying to take cordinates from inputes of user

--- brand/app/Destination.jsx
+++ brand/app/Destination.jsx
@@ -1,78 +1,77 @@
-// screens/Destination.js
-import React, { useState } from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { useRouter } from 'expo-router';
 import AddressPickup from './../components/AdressPickup';
 import CustomButton from '../components/CustomButton';
-import PropTypes from 'prop-types';
 
-const Destination = ({ route }) => {
-  const navigation = useNavigation();
-  const [coordinates, setCoordinates] = useState({
+const Destination = (props) => {
+  const router = useRouter();
+
+  const [state, setState] = useState({
     pickupCords: {},
     dropoffCords: {},
   });
 
   const onDone = () => {
-    if (route.params?.setCoordinates) {
-      route.params.setCoordinates({
-        pickupCords: coordinates.pickupCords,
-        dropoffCords: coordinates.dropoffCords,
-      });
-    }
-    navigation.goBack();
-  };
+    router.push({
+      pathname: '/ClientMap',
+      params: {
+        pickupCords: state.pickupCords,
+        dropoffCords: state.dropoffCords,
+      },
+    });
+  }
 
-  const fetchCords = (type, lat, lng) => {
-    setCoordinates((prevState) => ({
-      ...prevState,
-      [type]: { latitude: lat, longitude: lng },
-    }));
-  };
+  const fetchAddressCords = (lat, lng) => {
+    setState({
+      ...state,
+      pickupCords: {
+        latitude: lat,
+        longitude: lng,
+      },
+    });
+  }
+
+  const fetchDestinationCords = (lat, lng) => {
+    setState({
+      ...state,
+      dropoffCords: {
+        latitude: lat,
+        longitude: lng,
+      },
+    });
+  }
+
+  console.log("Pickup cords: ", state.pickupCords);
+  console.log("Dropoff cords: ", state.dropoffCords);
 
   return (
     <View style={styles.container}>
-      <ScrollView
+      <ScrollView 
         keyboardShouldPersistTaps="handled"
-        style={styles.scrollView}
+        style={{ backgroundColor: 'white', flex: 1, padding: 24 }}
       >
-        <AddressPickup
+        <AddressPickup 
           placeholderText="Enter Pickup location"
-          fetchAddress={(lat, lng) => fetchCords('pickupCords', lat, lng)}
+          fetchAddress={fetchAddressCords}
         />
-        <AddressPickup
+        <AddressPickup 
           placeholderText="Enter Destination location"
-          fetchAddress={(lat, lng) => fetchCords('dropoffCords', lat, lng)}
+          fetchAddress={fetchDestinationCords}
         />
-        <CustomButton
+        <CustomButton 
           btnTxt="Done"
           onPress={onDone}
-          btnStyle={styles.button}
+          btnStyle={{ marginTop: 24 }}
         />
       </ScrollView>
     </View>
   );
 };
 
-Destination.propTypes = {
-  route: PropTypes.shape({
-    params: PropTypes.shape({
-      setCoordinates: PropTypes.func,
-    }),
-  }).isRequired,
-};
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-  },
-  scrollView: {
-    flex: 1,
-    padding: 24,
-  },
-  button: {
-    marginTop: 24,
   },
 });
 
@@ -80,83 +79,83 @@ export default Destination;
 
 
 
-
-
-
-
-// import { View, StyleSheet, ScrollView } from 'react-native';
+// screens/Destination.js
 // import React, { useState } from 'react';
-// import { useNavigation } from '@react-navigation/native';
+// import { View, StyleSheet, ScrollView } from 'react-native';
+// import { useNavigation, useRoute } from '@react-navigation/native';
 // import AddressPickup from './../components/AdressPickup';
 // import CustomButton from '../components/CustomButton';
+// import PropTypes from 'prop-types';
 
-// const Destination = (props) => {
-
+// const Destination = ({ route }) => {
 //   const navigation = useNavigation();
-
-//   const [state, setState] = useState({
+//   const [coordinates, setCoordinates] = useState({
 //     pickupCords: {},
 //     dropoffCords: {},
 //   });
 
 //   const onDone = () => {
-//     props.params.getCordinates({
-//       pickupCords: state.pickupCords,
-//       dropoffCords: state.dropoffCords,
-//     })
-//     navigation.goBack();
-//   }
+//     if (route.params?.setCoordinates) {
+//       route.params.setCoordinates({
+//         pickupCords: coordinates.pickupCords,
+//         dropoffCords: coordinates.dropoffCords,
+//       });
+//     }
+//     navigation.navigate('ClientMap');
+//   };
 
-//   const fetchAddressCords = (lat, lng) => {
-//     setState({
-//       ...state,
-//       pickupCords: {
-//         latitude: lat,
-//         longitude: lng,
-//       }
-//     });
-//   }
-//   const fetchDestinationCords = (lat, lng) => {
-//     setState({
-//       ...state,
-//       dropoffCords: {
-//         latitude: lat,
-//         longitude: lng,
-//       }
-//     });
-//   }
-// console.log("props===>", props);
-//   // console.log("Pickup cords: ", state.pickupCords);
-//   // console.log("Dropoff cords: ", state.dropoffCords);
+//   const fetchCords = (type, lat, lng) => {
+//     setCoordinates((prevState) => ({
+//       ...prevState,
+//       [type]: { latitude: lat, longitude: lng },
+//     }));
+//   };
 
 //   return (
 //     <View style={styles.container}>
-//       <ScrollView 
+//       <ScrollView
 //         keyboardShouldPersistTaps="handled"
-//         style={{ backgroundColor: 'white', flex: 1, padding: 24 }}
+//         style={styles.scrollView}
 //       >
-//         <AddressPickup 
+//         <AddressPickup
 //           placeholderText="Enter Pickup location"
-//           fetchAddress={fetchAddressCords}
+//           fetchAddress={(lat, lng) => fetchCords('pickupCords', lat, lng)}
 //         />
-//         <AddressPickup 
+//         <AddressPickup
 //           placeholderText="Enter Destination location"
-//           fetchAddress={fetchDestinationCords}
+//           fetchAddress={(lat, lng) => fetchCords('dropoffCords', lat, lng)}
 //         />
-//         <CustomButton 
+//         <CustomButton
 //           btnTxt="Done"
 //           onPress={onDone}
-//           btnStyle={{ marginTop: 24 }}
+//           btnStyle={styles.button}
 //         />
 //       </ScrollView>
 //     </View>
 //   );
 // };
 
+// Destination.propTypes = {
+//   route: PropTypes.shape({
+//     params: PropTypes.shape({
+//       setCoordinates: PropTypes.func,
+//     }),
+//   }).isRequired,
+// };
+
 // const styles = StyleSheet.create({
 //   container: {
 //     flex: 1,
+//     backgroundColor: 'white',
+//   },
+//   scrollView: {
+//     flex: 1,
+//     padding: 24,
+//   },
+//   button: {
+//     marginTop: 24,
 //   },
 // });
 
 // export default Destination;
+
