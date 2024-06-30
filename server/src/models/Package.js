8dd172dd06c1a216395c041357c2f9559d8ae6a7
rasuliomari <rasuli707046@gmail.com>
2024-06-30 04:33:33 +0300

    updates of packages API

--- server/src/models/Package.js
+++ server/src/models/Package.js
@@ -2,11 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const packageSchema = new Schema({
-    senderId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    recipientId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    pickupAddress: { type: String, required: true },
-    deliveryAddress: { type: String, required: true },
-    status: { type: String, enum: ['pending', 'in-transit', 'delivered', 'cancelled'], required: true }
+    senderId: { type: Schema.Types.ObjectId, ref: 'User', required: false },
+    recipientId: { type: Schema.Types.ObjectId, ref: 'User', required: false },
+    pickuplocation: { type: String, required: true },
+    deliverylocation: { type: String, required: true },
+    fullName: { type: String, required: true },
+    PhoneNumber: { type: String, required: true },
+    Detail: { type: String, required: true },
+    Inside: { type: String, required: true },
+    rideType: { type: String, enum: ['bodaboda', 'kirikuu'], required: true },
+    cost: { type: Number, required: true },
+    status: { type: String, enum: ['pending', 'in-transit', 'delivered', 'cancelled'], required: false, default: 'pending' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Package', packageSchema);
+
+
+
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const packageSchema = new Schema({
+//     senderId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+//     recipientId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+//     pickupAddress: { type: String, required: true },
+//     deliveryAddress: { type: String, required: true },
+//     status: { type: String, enum: ['pending', 'in-transit', 'delivered', 'cancelled'], required: true }
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('Package', packageSchema);
