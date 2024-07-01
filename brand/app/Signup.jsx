47e60cd41ca6a6b1afc70d6075fcc73b5e2bf3d9
rasuliomari <rasuli707046@gmail.com>
2024-07-02 01:04:20 +0300

    changing IP

--- brand/app/Signup.jsx
+++ brand/app/Signup.jsx
@@ -59,7 +59,7 @@ const Signup = () => {
       role: values.role,
     };
 
-    const url = `http://192.168.62.127:4000/api/users/register`;
+    const url = `http://192.168.58.127:4000/api/users/register`;
 
     try {
       const response = await fetch(url, {
